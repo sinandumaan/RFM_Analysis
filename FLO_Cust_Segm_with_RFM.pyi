05246a9583d5bd9aa880800a22116ad0bbe508ca
sinandumaan <sinanduman1992@gmail.com>
2022-12-13 14:48:02 +0300

    RFM Analyze

--- FLO_Cust_Segm_with_RFM.pyi
+++ FLO_Cust_Segm_with_RFM.pyi
@@ -140,4 +140,43 @@ customer_ids_2 = df[(df["master_id"].isin(target_segments_customer_ids)) & ((df[
 customer_ids_2.to_csv("40_discount_Men_Children.csv", index=False)
 
 
+# Functionalize Customer Segmentation with RFM
+def cust_seg_with_rfm(dataframe):
+
+    dataframe["order_num_total"] = dataframe["order_num_total_ever_online"] + dataframe["order_num_total_ever_offline"]
+    dataframe["customer_value_total"] = dataframe["customer_value_total_ever_offline"] + dataframe["customer_value_total_ever_online"]
+    date_columns = dataframe.columns[dataframe.columns.str.contains("date")]
+    dataframe[date_columns] = dataframe[date_columns].apply(pd.to_datetime)
+
+    dataframe["last_order_date"].max()  # 2021-05-30
+    analysis_date = dt.datetime(2021, 6, 1)
+    rfm = pd.DataFrame()
+    rfm["customer_id"] = dataframe["master_id"]
+    rfm["recency"] = (analysis_date - dataframe["last_order_date"]).astype('timedelta64[D]')
+    rfm["frequency"] = dataframe["order_num_total"]
+    rfm["monetary"] = dataframe["customer_value_total"]
+
+    rfm["recency_score"] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
+    rfm["frequency_score"] = pd.qcut(rfm['frequency'].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
+    rfm["monetary_score"] = pd.qcut(rfm['monetary'], 5, labels=[1, 2, 3, 4, 5])
+    rfm["RF_SCORE"] = (rfm['recency_score'].astype(str) + rfm['frequency_score'].astype(str))
+    rfm["RFM_SCORE"] = (rfm['recency_score'].astype(str) + rfm['frequency_score'].astype(str) + rfm['monetary_score'].astype(str))
+
+    seg_map = {
+        r'[1-2][1-2]': 'hibernating',
+        r'[1-2][3-4]': 'at_Risk',
+        r'[1-2]5': 'cant_loose',
+        r'3[1-2]': 'about_to_sleep',
+        r'33': 'need_attention',
+        r'[3-4][4-5]': 'loyal_customers',
+        r'41': 'promising',
+        r'51': 'new_customers',
+        r'[4-5][2-3]': 'potential_loyalists',
+        r'5[4-5]': 'champions'
+    }
+    rfm['segment'] = rfm['RF_SCORE'].replace(seg_map, regex=True)
+
+    return rfm[["customer_id", "recency","frequency","monetary","RF_SCORE","RFM_SCORE","segment"]]
+
+rfm_df = cust_seg_with_rfm(df)
 
