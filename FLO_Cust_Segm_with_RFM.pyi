28db1d33d61d125f85a6c652e29a9b5efc03d185
sinandumaan <sinanduman1992@gmail.com>
2022-12-13 14:02:24 +0300

    RFM Analyze

--- FLO_Cust_Segm_with_RFM.pyi
+++ FLO_Cust_Segm_with_RFM.pyi
@@ -16,9 +16,47 @@ pd.set_option('display.width',1000)
 df_ = pd.read_csv("flo_data_20k.csv")
 df = df_.copy()
 
+# Variables summary
+
 df.columns
 df.describe().T
 df.isnull().sum()
 df.info()
+
+# The number of total orders and total customer values are defined
+
+df["order_num_total"] = df["order_num_total_ever_online"] + df["order_num_total_ever_offline"]
+df["customer_value_total"] = df["customer_value_total_ever_offline"] + df["customer_value_total_ever_online"]
+
+# Variable which contains "date" is converted to datetime datatype
+
+date_columns = df.columns[df.columns.str.contains("date")]
+df[date_columns] = df[date_columns].apply(pd.to_datetime)
+
+# The distribution of the number of customers in the shopping channels, the total number of products purchased and the total expenditures.
+
+df.groupby("order_channel").agg({"master_id":"count",
+                                 "order_num_total":"sum",
+                                 "customer_value_total":"sum"})
+
+# The top 10 customers with the highest customer value
+
+df.sort_values("customer_value_total", ascending=False)[:10]
+
+# The top 10 customers with the most orders.
+
+df.sort_values("order_num_total", ascending=False)[:10]
+
+# Functionalize the Data Preparation section
+def data_prep(dataframe):
+    dataframe["order_num_total"] = dataframe["order_num_total_ever_online"] + dataframe["order_num_total_ever_offline"]
+    dataframe["customer_value_total"] = dataframe["customer_value_total_ever_offline"] + dataframe["customer_value_total_ever_online"]
+    date_columns = dataframe.columns[dataframe.columns.str.contains("date")]
+    dataframe[date_columns] = dataframe[date_columns].apply(pd.to_datetime)
+    return df
+
+
+
+
 
 
